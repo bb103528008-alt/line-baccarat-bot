7adfff6778134a4bd453823442d262fd197f1634
Peng. <bb103528008@gmail.com>
2026-05-10 22:46:06 +0800

    get group id

--- app.py
+++ app.py
@@ -221,6 +221,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print("GROUP_ID:", event.source.group_id)
+
+    
     text = event.message.text.strip()
 
     if text == "/查詢":
